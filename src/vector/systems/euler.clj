8b06e0f647de49ca22a1a4d252cdb6cc20dfcc88
Walter P Babic <wbabic@mac.com>
2015-04-01 05:24:17 -0600

    euler systems

--- src/vector/systems/euler.clj
+++ src/vector/systems/euler.clj
@@ -29,6 +29,23 @@
        (>! out [:end])))
    out))
 
+(defn f
+  [x]
+  (format "%7.3f"  (double x)))
+
+(defn print-point
+  [[x y]]
+  (println
+   "x: " (f x) "y: "(f y)))
+
+(defn print-step
+  "format and print a vector"
+  [k t x y]
+  (println
+   (format "%3d" k)
+   (format "%5.2f"  (double t))
+   (f x) (f y)))
+
 (defn print-results
   [results]
   (go-loop []
@@ -36,17 +53,16 @@
            [:start]
            (do
              (println "")
-             (println "start")
              (recur))
            [:step k t [x y]]
            (do
-             (println
-              (format "%3d" k)
-              (format "%5.2f"  (double t))
-              (format "%7.3f"  (double x))
-              (format "%7.3f"  (double y)))
+             (print-step k t x y)
              (recur))
            [:end] (println "end"))))
+
+(defn distance
+  [v w]
+  (m/distance v w))
 
 (defn compare-approximation
   [config results]
@@ -57,26 +73,23 @@
       (match (<! results)
              [:start] (do
                         (println "")
-                        (println "start")
                         (recur))
              [:step k t [x y]]
-             (do (println
-                  (format "%3d" k)
-                  (format "%5.2f"  (double t))
-                  (format "%7.3f"  (double x))
-                  (format "%7.3f"  (double y)))
-                 (when (check-points t)
-                   (println "checkpoint")
-                   (let [s (solution t)
-                         d (distance s [x y])]
-                     (println s)
-                     (println d)))
-                 (recur))
+             (do
+               (print-step k t x y)
+               (when (check-points t)
+                 (println "checkpoint")
+                 (let [s (solution t)
+                       d (distance s [x y])
+                       l1 (distance s [0 0])
+                       l2 (distance [x y] [0 0])]
+                   (print "solution: ")
+                   (print-point s)
+                   (println "distance: " (f d))
+                   (println "l1: " (f l1))
+                   (println "l2: " (f l2))))
+               (recur))
              [:end] (println "end")))))
-
-(defn distance
-  [v w]
-  (m/distance v w))
 
 (def ex1
   {:step-size 0.25
@@ -88,47 +101,56 @@
 (def ex2
   {:diff-eq (fn [[x y]] [(- y) x])
    :solution (fn [t] [(Math/cos t) (Math/sin t)])
-   :check-points #{4.0 6.0 10.0}
+   :check-points #{4 6 10}
    :initial-condition [1 0]
    :ti 0
    :tf 10
-   :step-size 0.5})
+   :step-size (/ 2)})
 
-(def ex2b (assoc ex2 :step-size 0.1))
+(def ex2b (assoc ex2 :step-size (/ 10)))
 
 (def hw1
   {:diff-eq (fn [[x y]] [y (- (Math/sin x))])
    :initial-condition [0 2]
-   :step-size 0.25
+   :step-size (/ 4)
    :ti 0
-   :tf (* 0.25 8)})
+   :tf (* (/ 4) 8)})
 
 (def hw2
-  {:diff-eq (fn [[x y]] [(+ y (* y y)) (+ (- x) (/ y 5) (* -1 x y) (* 6/5 y y))])
+  {:diff-eq (fn [[x y]]
+              [(+ y (* y y))
+               (+ (- x) (/ y 5) (* -1 x y) (* 6/5 y y))])
    :initial-condition [1 1]
-   :step-size 0.25
+   :step-size (/ 4)
    :ti 0
-   :tf (* 0.25 5)})
+   :tf (* (/ 4) 5)})
 
 (def hw3
   {:diff-eq (fn [[x y]] [(* 2 x) y])
    :initial-condition [1 3]
-   :step-size 0.5
-   :solution (fn [t] [(Math/exp (* 2 t)) (Math/exp (* 3 t))])
+   :step-size (/ 2)
+   :solution (fn [t] [(Math/exp (* 2 t)) (* 3 (Math/exp t))])
    :check-points #{2 4 6}
    :ti 0
    :tf 6})
 
-(def hw3b (assoc hw3 :step-size 0.1))
+(def hw3b (assoc hw3 :step-size (/ 10)))
 
 (def hw4
   {:diff-eq (fn [[y v]] [v (+ (* -1/2 v) (* -4 y))])
    :initial-condition [2 0]
-   :step-size 0.25
+   :step-size (/ 4)
    :ti 0
    :tf 10})
 
-(def hw4b (assoc hw4 :step-size 0.1))
+(defn to-vec
+  [config]
+  (let [{:keys [ti tf step-size diff-eq initial-condition]} config
+        num-steps (/ (- tf ti) step-size)
+        next (fn [p] (next-step p step-size diff-eq))]
+    (into [] (take num-steps (iterate next initial-condition)))))
+
+(def hw4b (assoc hw4 :step-size (/ 10)))
 
 (comment
   (require '[vector.systems.euler :as e] :reload)
@@ -136,8 +158,18 @@
   (def l (fn [p] (e/next-step p 0.25 F)))
   (take 5 (iterate l [1 1]))
 
+  (e/print-results (e/euler-loop e/ex1))
   (e/print-results (e/euler-loop e/ex2))
   (e/compare-approximation e/ex2 (e/euler-loop e/ex2))
+  (e/compare-approximation e/ex2b (e/euler-loop e/ex2b))
   (e/print-results (e/euler-loop e/hw1))
   (e/print-results (e/euler-loop e/hw2))
+  (e/print-results (e/euler-loop e/hw3))
+  (e/print-results (e/euler-loop e/hw3b))
+  (e/compare-approximation e/hw3 (e/euler-loop e/hw3))
+  (e/compare-approximation e/hw3b (e/euler-loop e/hw3b))
+  (e/print-results (e/euler-loop e/hw4))
+
+  (spit "hw4.clj" (pr-str (e/to-vec e/hw4)))
+  (read-string (slurp "hw4.clj"))
   )
