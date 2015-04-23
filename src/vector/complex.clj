e1d6399205e8322c59c6d62f1009a5aa02ce6404
Walter P Babic <wbabic@mac.com>
2015-04-23 01:26:21 -0600

    added to complex

--- src/vector/complex.clj
+++ src/vector/complex.clj
@@ -1,4 +1,5 @@
-(ns vector.complex)
+(ns vector.complex
+  (:require [clojure.math.numeric-tower :as math]))
 
 ;; the field of complex numbers
 ;; where each number is represented as a vector
@@ -9,6 +10,10 @@
 (def i [0 1])
 (defn add [[x1 y1] [x2 y2]]
   [(+ x1 x2) (+ y1 y2)])
+(defn negate [[x y]]
+  [(- x) (- y)])
+(defn minus [z w]
+  (add z (negate w)))
 (defn mult [[x1 y1] [x2 y2]]
   [(- (* x1 x2) (* y1 y2))
    (+ (* x1 y2) (* x2 y1))])
@@ -21,6 +26,11 @@
   (let [[x y] z
         f (/ (det z))]
     [(* f x) (* f y -1)]))
+
+(defn div
+  "return z/w"
+  [z w]
+  (mult z (mult-inverse w)))
 
 ;; laws - check via test.check
 ;; associative law: (z1 * (z2 * z3)) = ((z1 * z2) * z3)
@@ -35,9 +45,9 @@
   (det z))
 (defn modulus [z]
   (let [ds (modulus-squared z)]
-    (Math/sqrt ds)))
+    (math/sqrt ds)))
 (defn argument [[x y]]
-  (Math/atan2 (/ y x)))
+  (Math/atan2 y x))
 (defn conjugate [[x y]]
   [x (* -1 y)])
 
@@ -64,6 +74,22 @@
 ;; mod (z1 + z2) <= mod z1 + mod z2
 ;; proof
 
+(defn pow
+  "raise complex number z to the nth power
+  where n is given ineger"
+  [z n]
+  (assert (integer? n) "n must be an integer")
+  (loop [w z n n]
+    (cond (zero? n) one
+           (= 1 n) w
+           :else (recur (mult w z) (dec n)))))
+
+(defn powers
+  "return list of powers of z from 0 to n"
+  [z n]
+  (let [f (partial pow z)]
+    (map f (range 0 (inc n)))))
+
 (comment
   (require '[vector.complex :as complex] :reload)
   (complex/add complex/one complex/i)
@@ -72,4 +98,21 @@
   ;;=> true
   (complex/mult-inverse complex/i)
   ;;=> [0 -1]
+
+  (let [w [(/ 2) (/ 2)]
+        f (fn [z] (complex/mult w z))]
+    (take 8 (iterate f w)))
+  ;;=> ([1/2 1/2] [0N 1/2] [-1/4 1/4] [-1/4 0N] [-1/8 -1/8] [0N -1/8] [1/16 -1/16] [1/16 0N])
+
+  (let [w [(/ 2) (/ 2)]]
+    (for [n (range 1 9)]
+      (reduce complex/add (complex/powers w n))))
+  ;;=> ([3/2 1/2] [3/2 1N] [5/4 5/4] [1N 5/4] [7/8 9/8] [7/8 1N] [15/16 15/16] [1N 15/16])
+
+  ;; (1 - z^(n+1))/(1 - z)
+  (let [w [(/ 2) (/ 2)]
+        f (fn [n] (complex/div (complex/minus complex/one (complex/pow w (inc n)))
+                                 (complex/minus complex/one w)))]
+    (map f (range 1 9)))
+  ;;=> ([3/2 1/2] [3/2 1N] [5/4 5/4] [1N 5/4] [7/8 9/8] [7/8 1N] [15/16 15/16] [1N 15/16])
   )
